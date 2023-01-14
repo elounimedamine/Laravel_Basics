89b7fbc66dff4a628327dffb0b4bb4c570455a1e
Amine <medamineelouni1@gmail.com>
2023-01-14 18:43:13 +0100

    First_Touch_VD3

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//3rd First Touch Video Routing
+Route::get("/page1", function(){
+    return view("page1");
+});
